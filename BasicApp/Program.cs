0429edb4a903257453a337278747a96a183419f5
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-09-04 08:06:29 +0545

    Learn about datatypes

--- BasicApp/Program.cs
+++ BasicApp/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// OOP: Object Oriented Programming with C#
+// C#: Multi Paradigm Language
+// Static vs Dynamic
+// Procedural vs OOP vs Functional
+// Imperative vs Declarative
+
+// Datatypes
+// Numbers
+// Integers
+byte personAge = 45;
+short students = 2342;
+int coutryPopulation = 2342432;
+long weightOfEarth = 232353354353;
+
+// Floating point numbers: precision (accuracy)
+float elephantWeight = 34354.4564562342424324234f;
+double freezerPrice = 345345.345345532344234234;
+decimal moleculeWeight = 43.342324423424234242m;
+
+// Text
+string collegeName = "NAST";
+char gender = 'M';
+
+// Boolean
+bool isAbscentToday = true;
+
+// Custom
+DateTime dob = DateTime.Now;
+
+var fullName = 89;          // Type inference
+int? marks = null;             // All types
+marks = 43;
+
+Console.WriteLine(personAge);
