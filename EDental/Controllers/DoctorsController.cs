5f335eb7890b07d2480e53f87584459dfa36e355
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-10-16 08:20:00 +0545

    Add new doctor form

--- EDental/Controllers/DoctorsController.cs
+++ EDental/Controllers/DoctorsController.cs
@@ -1,9 +1,11 @@
 
 using EDental.Data;
+using EDental.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 
 public class DoctorsController: Controller
 {
+    [HttpGet]
     public IActionResult Index()
     {
         // Fetch data from doctor table
@@ -14,5 +16,22 @@ public class DoctorsController: Controller
         var doctors = db.Doctor.ToList(); // select * from doctor
 
         return View(doctors);
+    }
+
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Add(Doctor doctor)
+    {
+        // Save to db
+        var db = new EDentalDbContext();
+        db.Doctor.Add(doctor);
+        db.SaveChanges();
+    
+        return RedirectToAction(nameof(Index));
     }
 }
---
+++ EDental/Data/Scripts/Doctor.sql
@@ -0,0 +1,17 @@
+﻿create table Doctor
+(
+	Id int primary key identity(1,1),
+	FullName nvarchar(100) not null,
+	Address nvarchar(200) not null,
+	Phone varchar(20) null, 
+	NDCNumber nvarchar(100) not null, 
+	Qualification varchar(200), 
+	Specialization varchar(200), 
+	Gender int, 
+	DOB Datetime2
+)
+
+insert into Doctor
+values ('Dr. James', 'Kathmandu, Nepal', '9841234563', 'ABC34784FG', 'MDS MS', 'Dental Surgery', 1, '1980/12/12')
+
+select * from Doctor
