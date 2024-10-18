077e9227c8ea4f6ad80eba229633e5f5ce893083
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-10-18 08:03:56 +0545

    Implement UD of CRUD for Doctors

--- EDental/Controllers/DoctorsController.cs
+++ EDental/Controllers/DoctorsController.cs
@@ -34,4 +34,44 @@ public class DoctorsController: Controller
     
         return RedirectToAction(nameof(Index));
     }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var db = new EDentalDbContext();
+        var doctor = db.Doctor.Find(id);
+
+        return View(doctor);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Doctor doctor)
+    {
+        // Save to db
+        var db = new EDentalDbContext();
+        db.Doctor.Update(doctor);
+        db.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var db = new EDentalDbContext();
+        var doctor = db.Doctor.Find(id);
+
+        return View(doctor);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(Doctor doctor)
+    {
+        // Save to db
+        var db = new EDentalDbContext();
+        db.Doctor.Remove(doctor);
+        db.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
 }
